718985e94433a14d6230e5fd966fee383a7f1a56
Joaquín Caro <me@joaquin-caro.es>
2018-07-31 00:17:57 +0200

    add desc to test expect

--- MarsRover.Tests/CompassTest.fs
+++ MarsRover.Tests/CompassTest.fs
@@ -8,15 +8,15 @@ let tests =
   testList "About Rotate Compass" [
     testCase "given a compass pointing to North when rotate to left orientation will be West" <| fun () ->
         let compass = Compass.North
-        Expect.equal compass.RotateLeft Compass.West "2+2 = 4"
+        Expect.equal compass.RotateLeft Compass.West "From North to left = West"
      
     testCase "given a compass pointing to North when rotate to right orientation will be Est" <| fun () ->
         let compass = Compass.North
-        Expect.equal compass.RotateRight Compass.East "2+2 = 4"
+        Expect.equal compass.RotateRight Compass.East "From North to Rigth = East"
 
     testCase "given a compass pointing to East when rotate to right orientation will be SOUTH" <| fun () ->
         let compass = Compass.East
-        Expect.equal compass.RotateRight Compass.South "2+2 = 4"
+        Expect.equal compass.RotateRight Compass.South "From East to Right = South"
     
   
   ]
