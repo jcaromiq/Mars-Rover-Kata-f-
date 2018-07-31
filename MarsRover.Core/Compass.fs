c7450bed54b45e2468ae26fac09ce26449a63c01
Joaquin Caro <joaquin.caro@apiumhub.com>
2018-07-31 17:13:29 +0200

    indent

--- MarsRover.Core/Compass.fs
+++ MarsRover.Core/Compass.fs
@@ -1,46 +1,42 @@
 namespace MarsRover.Core
+
 module Compass =
 
-    [<StructuralEquality;StructuralComparison>]
-    type Orientation =  
+    [<StructuralEquality; StructuralComparison>]
+    type Orientation =
         | NORTH
         | EAST
-        | SOUTH 
+        | SOUTH
         | WEST
-
-    let private cardinalPoints = [Orientation.NORTH; 
-        Orientation.EAST;
-        Orientation.SOUTH;
-        Orientation.WEST]
-
+    
+    let private cardinalPoints = [ Orientation.NORTH; Orientation.EAST; Orientation.SOUTH; Orientation.WEST ]
+    
     type Compass(orientation : Orientation) =
         member this.Orientation = orientation
         static member North = Compass(NORTH)
         static member West = Compass(WEST)
         static member East = Compass(EAST)
         static member South = Compass(SOUTH)
-
-        member this.RotateLeft = 
-            cardinalPoints 
+        
+        member this.RotateLeft =
+            cardinalPoints
             |> List.rev
             |> this.Rotate
-
-        member this.RotateRight = 
-            cardinalPoints
-            |> this.Rotate
-
-        member private this.Rotate(list) = 
-            list 
-            |> List.findIndex(fun i -> i = orientation) 
-            |> (fun i -> (i + 1) % list.Length) 
+        
+        member this.RotateRight = cardinalPoints |> this.Rotate
+        
+        member private this.Rotate(list) =
+            list
+            |> List.findIndex (fun i -> i = orientation)
+            |> (fun i -> (i + 1) % list.Length)
             |> (fun i -> list.Item(i))
             |> (fun o -> Compass(o))
-
+        
         override x.ToString() = orientation.ToString()
-        override x.GetHashCode() =
-           hash (orientation)
-
-        override x.Equals(b) = 
+        
+        override x.GetHashCode() = hash (orientation)
+        
+        override x.Equals(b) =
             match b with
-               | :? Compass as c -> (orientation) = (c.Orientation)
-               | _ -> false
+            | :? Compass as c -> (orientation) = (c.Orientation)
+            | _ -> false
