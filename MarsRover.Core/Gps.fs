7e0a9be2bc5d49d1d0630e41ab4e189ac55dc724
Joaquin Caro <joaquin.caro@apiumhub.com>
2018-08-03 00:52:14 +0200

    Move around x & y

--- MarsRover.Core/Gps.fs
+++ MarsRover.Core/Gps.fs
@@ -11,16 +11,32 @@ namespace MarsRover.Core
         let private incrementX position:Position = {X=position.X+1; Y=position.Y}
         let private decrementX position:Position = {X=position.X-1; Y=position.Y}
 
+        let private incrementY position:Position = {X=position.X; Y=position.Y+1}
+
+        let private decrementY position:Position = {X=position.X; Y=position.Y-1}
+
                 
         type Gps (position:Position, compass:Compass) =
             member this.Position = position
 
             member this.Compass = compass
 
-            member this.Forward = new Gps(incrementX this.Position, compass=Compass.East)
-
-            member this.Backward = new Gps(decrementX this.Position, compass=Compass.East)
+            member this.Forward = 
+                match this.Compass.Orientation with
+                    | Orientation.EAST -> new Gps(incrementX this.Position, compass=Compass.East)
+                    | Orientation.WEST -> new Gps(decrementX this.Position, compass=Compass.West)
+                    | Orientation.NORTH -> new Gps(incrementY this.Position, compass=Compass.North)
+                    | Orientation.SOUTH -> new Gps(decrementY this.Position, compass=Compass.South)
+                      
            
+            member this.Backward = 
+                match this.Compass.Orientation with
+                    | Orientation.EAST -> new Gps(decrementX this.Position, compass=Compass.East)
+                    | Orientation.WEST -> new Gps(incrementX this.Position, compass=Compass.West)
+                    | Orientation.NORTH -> new Gps(decrementY this.Position, compass=Compass.North)
+                    | Orientation.SOUTH -> new Gps(incrementY this.Position, compass=Compass.South)
+
+         
             override this.ToString() = this.Compass.ToString() + "{" + this.Position.ToString()  + "}"
         
             override this.GetHashCode() = hash (this.ToString())
--- MarsRover.Tests/GpsTest.fs
+++ MarsRover.Tests/GpsTest.fs
@@ -19,4 +19,45 @@ let moveOverXTest =
 
         Expect.equal gps.Backward movement "should decrement x in one"
 
+    testCase "when is oriented to WEST and go forward" <| fun () ->
+        let gps = create(1,0,Compass.West)
+        let movement = create(0,0,Compass.West)
+
+        Expect.equal gps.Forward movement "should decrement x in one"
+    
+    testCase "when is oriented to WEST and go back" <| fun () ->
+        let gps = create(0,0,Compass.West)
+        let movement = create(1,0,Compass.West)
+
+        Expect.equal gps.Backward movement "should increment x in one"
+
   ]
+let moveOverYTest =
+  testList "Given a orientation in Y axis" [
+    testCase "when is oriented to North and go forward" <| fun () ->
+        let gps = create(0,0,Compass.North)
+        let movement = create(0,1,Compass.North)
+
+        Expect.equal gps.Forward movement "should increment y in one"
+
+    testCase "when is oriented to NORTH and go back" <| fun () ->
+        let gps = create(0,1,Compass.North)
+        let movement = create(0,0,Compass.North)
+
+        Expect.equal gps.Backward movement "should decrement y in one"
+
+    testCase "when is oriented to SOUTH and go forward" <| fun () ->
+        let gps = create(0,1,Compass.South)
+        let movement = create(0,0,Compass.South)
+
+        Expect.equal gps.Forward movement "should decrement y in one"
+    
+    testCase "when is oriented to SOUTH and go back" <| fun () ->
+        let gps = create(0,0,Compass.South)
+        let movement = create(0,1,Compass.South)
+
+        Expect.equal gps.Backward movement "should increment y in one"
+
+  ]
+
+  
